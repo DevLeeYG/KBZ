ee5cceeea89c0e1b55b39dd0b4f8779fcc17f26a
hi33741 <hi33741@naver.com>
2022-02-15 16:52:12 +0900

    headFrame

--- components/Home/HomeHead/HomeHad.tsx
+++ components/Home/HomeHead/HomeHad.tsx
@@ -5,18 +5,82 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { headstyle } from '../../../makeStyles/Homehead';
+import { CardMedia } from '@mui/material';
 const HomeHad = () => {
   const classes = headstyle();
 
   return (
     <Box>
-      <AppBar className={classes.root} position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
+      <AppBar position="static">
+        <Box className={classes.root}>
+          <Box
+            sx={{ border: '1px solid black', width: '33.3%', height: '80px' }}
+          >
+            <Box className={classes.headLeft} component="div">
+              <Typography
+                className={classes.headLeftInner}
+                variant="h6"
+                component="div"
+              >
+                김반장
+              </Typography>
+              <Typography
+                className={classes.headLeftInner}
+                variant="h6"
+                component="div"
+              >
+                업무요청
+              </Typography>
+              <Typography
+                className={classes.headLeftInner}
+                variant="h6"
+                component="div"
+              >
+                게시판
+              </Typography>
+            </Box>
+            <Box></Box>
+            <Box></Box>
+          </Box>
+          <Box
+            sx={{
+              border: '1px solid black',
+              width: '33.3%',
+              height: '80px',
+            }}
+          >
+            <Box
+              sx={{
+                height: '80px',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <CardMedia
+                className={classes.media}
+                component="img"
+                image="/new_Logo.webp"
+              />
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'right',
+
+              border: '1px solid black',
+              width: '33.3%',
+              height: '80px',
+            }}
+          >
+            <Box>
+              <Button sx={{ height: '80px', width: '130px' }} color="inherit">
+                유저이름
+              </Button>
+            </Box>
+          </Box>
+        </Box>
       </AppBar>
     </Box>
   );
--- makeStyles/Homehead.tsx
+++ makeStyles/Homehead.tsx
@@ -3,8 +3,27 @@ import { makeStyles } from '@material-ui/core';
 export const headstyle = makeStyles((theme) => ({
   root: {
     display: 'flex',
-    justifyContent: 'center',
+    width: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     height: '80px',
     backgroundColor: '#294ec7',
+  },
+  headLeft: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '300px',
+    height: '80px',
+  },
+  headLeftInner: {
+    marginLeft: '15px',
+    marginRight: '15px',
+  },
+  mediaBox: {
+    position: 'relative',
+  },
+  media: {
+    width: '180px',
   },
 }));
